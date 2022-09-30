c77d94632d92d0c7d50999f92e811273c9e6a2a7
gemaquejr <gemaquejr@hotmail.com>
2022-09-30 19:33:46 -0300

    Finalizado 04_insertMatches

--- app/backend/src/routes/matches.ts
+++ app/backend/src/routes/matches.ts
@@ -2,18 +2,18 @@ import { Router } from 'express';
 import MatchesController from '../controllers/matchController';
 import MatchesService from '../services/matchService';
 import MatchesRepository from '../repository/matchRepository';
-// import Jwt from '../middlewares/auth';
+import Jwt from '../middlewares/auth';
 
 const route = Router();
 
-// const jwt = new Jwt();
+const jwt = new Jwt();
 
 const matches = new MatchesRepository();
 const matchesService = new MatchesService(matches);
 const matchesController = new MatchesController(matchesService);
 
 route.get('/matches', matchesController.getMatches.bind(matchesController));
-route.post('/matches', matchesController.postMatches.bind(matchesController));
+route.post('/matches', jwt.validate, matchesController.postMatches.bind(matchesController));
 route.patch('/matches/:id/finish', matchesController.patchMatch.bind(matchesController));
 
 export default route;
