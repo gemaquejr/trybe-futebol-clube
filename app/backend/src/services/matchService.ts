b5e303b9e5bc8115b0aafb1f661f41f86259f30e
gemaquejr <gemaquejr@hotmail.com>
2023-03-27 20:51:51 -0300

    refatoracao de matchService.ts

--- app/backend/src/services/matchService.ts
+++ app/backend/src/services/matchService.ts
@@ -1,28 +1,56 @@
-import { IMatches, IMatchesModel } from '../interface/IMatch';
+import Team from '../database/models/team';
+import Match from '../database/models/match';
+import IMatch from '../interface/IMatch';
+import ICreateMatch from '../interface/ICreateMatch';
 
-export default class MatchesService {
-  constructor(private model: IMatchesModel) {
-    this.model = model;
-  }
+export default class MatchService {
+    public matchModel = Match;
 
-  public async getMatches(): Promise<IMatches[]> {
-    const matches = await this.model.getAllMatches();
-    return matches;
-  }
+    public async getAllMatches() {
+        const result = await this.matchModel.findAll({
+            include: [
+                { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+                { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+            ] } );
+        return result;
+    }
+    public async getMatchesInProgress(inProgress: boolean) {
+        const result = await this.matchModel.findAll({
+            where: { inProgress },
+            include: [
+                { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+                { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+            ] }, );
+            return result;
+    }
 
-  public async postMatches(data: IMatches): Promise<IMatches | boolean> {
-    const match = await this.model.postOneMatch(data);
-    return match;
-  }
-
-  public async patchMatch(id: number): Promise<void> {
-    const match = await this.model.patchOneMatch(id);
-    return match;
-  }
-
-  public async patchMatchInProgress(id: number, homeTeamGoals: number, awayTeamGoals: number):
-  Promise<void> {
-    const match = await this.model.patchOneMatchInProgress(id, homeTeamGoals, awayTeamGoals);
-    return match;
-  }
+    public async findTeam(match: ICreateMatch) {
+        const validMatch = await this.matchModel.findOne({ where: { id: match.homeTeam || match.awayTeam } });
+        return validMatch;
+    }
+    public async createMatch(match: ICreateMatch): Promise<object> {
+        if (match.homeTeam === match.awayTeam) {
+            throw new Error('It is not possible to create a match with two equal teams');
+        }
+        const result = await this.matchModel.create({
+            ...match,
+        });
+        return result as IMatch;
+    }
+    public async updateMatch(id: number): Promise<boolean> {
+        const result = await this.matchModel.update({ inProgress: false }, {
+            where: { id }
+        });
+        if (!result) {
+            return false
+        }
+        return true;
+    }
+    public async updateMatchInProgress(id: number, homeGoals: number, awayGoals: number): Promise<boolean> {
+        const result = await this.matchModel.update({
+            homeTeamGoals: homeGoals,
+            awayTeamGoals: awayGoals,
+        }, { where: { id, inProgress: true } } );
+        return true;
+    }
 }
