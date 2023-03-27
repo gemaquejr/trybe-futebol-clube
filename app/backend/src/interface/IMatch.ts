9e57271248432505131db323476ab66e618eac3c
gemaquejr <gemaquejr@hotmail.com>
2023-03-27 20:49:05 -0300

    Adiciona refatoracao de IMatch.ts em interface

--- app/backend/src/interface/IMatch.ts
+++ app/backend/src/interface/IMatch.ts
@@ -1,22 +1,8 @@
-export interface IMatches {
-  id?: number;
+export default interface IMatches {
+  id: number;
   homeTeam: number;
   homeTeamGoals: number;
   awayTeam: number;
   awayTeamGoals: number;
   inProgress: boolean;
-}
-
-export interface IMatchesService {
-  getMatches(): Promise<IMatches[]>;
-  postMatches(data: IMatches): Promise<IMatches | boolean>;
-  patchMatch(id: number): Promise<void>;
-  patchMatchInProgress(id: number, homeTeamGoals: number, awayTeamGoals: number): Promise<void>;
-}
-
-export interface IMatchesModel {
-  getAllMatches(): Promise<IMatches[]>;
-  postOneMatch(data: IMatches): Promise<IMatches | boolean>;
-  patchOneMatch(id: number): Promise<void>;
-  patchOneMatchInProgress(id: number, homeTeamGoals: number, awayTeamGoals: number): Promise<void>;
 }
