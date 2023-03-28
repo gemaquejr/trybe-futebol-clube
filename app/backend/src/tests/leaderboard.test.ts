1029cd1b025c610640d2dde7ecc1419d05df1364
gemaquejr <gemaquejr@hotmail.com>
2023-03-28 09:23:09 -0300

    Adiciona leaderboard.test.ts

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,40 @@
+import * as Sinon from 'sinon';
+import * as chai from 'chai';
+
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const testLogin = {
+  email: 'admin@admin.com',
+  password: 'secret_admin'
+}
+
+const { expect } = chai;
+
+describe('Leaderboard', () => {
+  beforeEach(() => Sinon.restore());
+  describe('Get route /, home, away', () => {
+    it('/leaderboard', async () => {
+        const response = await chai.request(app).get('/leaderboard');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.length.above(2);
+        expect(response.body).to.be.an('array');
+    })
+    it('/leaderboard/home', async () => {
+        const response = await chai.request(app).get('/leaderboard/home');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.length.above(2);
+        expect(response.body).to.be.an('array');
+    })
+    it('/leaderboard/away', async () => {
+        const response = await chai.request(app).get('/leaderboard/away');
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.length.above(2);
+        expect(response.body).to.be.an('array');
+    })
+  })
+});
