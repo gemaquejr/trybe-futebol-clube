2b17dbbca9b378a8ef6a0b1d882091f175cf8dc4
gemaquejr <gemaquejr@hotmail.com>
2023-03-28 09:01:12 -0300

    Adiciona index.ts

---
+++ app/backend/src/routes/index.ts
@@ -0,0 +1,6 @@
+import userRouter from './userRoute';
+import teamRouter from './teamRoute';
+import matchRouter from './matchRoute';
+import boardRouter from './leaderboardRoute'
+
+export default { userRouter, teamRouter, matchRouter, boardRouter };
