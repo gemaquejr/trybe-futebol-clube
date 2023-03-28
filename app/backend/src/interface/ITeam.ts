53c2f3bcd5d7774d799a158581102c295784f387
gemaquejr <gemaquejr@hotmail.com>
2023-03-27 21:02:16 -0300

    Adiciona refatoracao de ITeam.ts em interface

--- app/backend/src/interface/ITeam.ts
+++ app/backend/src/interface/ITeam.ts
@@ -1,14 +1,4 @@
-export interface ITeam {
+export default interface ITeam {
   id: number;
   teamName: string;
-}
-
-export interface ITeamService {
-  getTeams(): Promise<ITeam[]>;
-  getTeamById(id: number): Promise<ITeam | null> ;
-}
-
-export interface ITeamModel {
-  getAllTeams(): Promise<ITeam[]>;
-  getOneTeamById(id: number): Promise<ITeam | null> ;
 }
